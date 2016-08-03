5de00ce94ea8c76a7bf792cc576b1e0f427dfb2b
nethalyg <nethalyg@hotmail.com>
2016-08-03 13:18:38 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-exports.url = 'morning-island-80076.herokuapp.com';
+exports.url = 'http://morning-island-80076.herokuapp.com';
 exports.senderEmail = "nethaly@keysource.us";
 exports.senderName = "Nethaly Test";
 exports.listID = 21041;
